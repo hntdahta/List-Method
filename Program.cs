98dc6f588132ba428a56f5a93c5dc1e8616f91bb
Thanh Dat <datmathoingu1100@gmail.com>
2020-09-30 16:33:38 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> MyList = new List<string>();
+            MyList.Add("lion");
+            MyList.Add("cat");
+            MyList.Add("dog");
+            MyList.Add("elephant");
+            foreach (var item in MyList)
+            {
+                Console.WriteLine(item);
+            }
+            MyList.Remove("dog");
+            foreach (var item in MyList)
+            {
+                Console.WriteLine(item);
+            }
+            var animal = from name in MyList
+                         orderby name.Length
+                         select name;
+            foreach (var item in animal)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Cat index :");
+            Console.WriteLine(MyList.IndexOf("cat"));
+        }
+    }
+}
